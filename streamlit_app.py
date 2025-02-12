78cdc939066155023d9a8da164c1f2ef5ab3d2f8
markusv269 <60115561+markusv269@users.noreply.github.com>
2025-02-12 20:03:50 +0000

    new file:   Pictures/SL_logo.png modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-st.title("SL Redraft 2024 -- Wochenauswertung :football:")
+st.title("Stoned Lack Redraft 2024 -- Wochenauswertung :football:")
 st.write(
     "Wöchentlicher Überblick über die SLR 2024. 35 Ligen, 420 Manager, eine App."
 )
+
+st.logo("Pictures/SL_logo", link=None, size='medium')
