5a61f9e709bfae7958d501fd45703c96ca103f65
markusv269 <60115561+markusv269@users.noreply.github.com>
2025-02-12 20:26:25 +0000

    modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.subheader(
     "Wöchentlicher Überblick über die SLR 2024. 35 Ligen, 420 Manager, eine App."
 )
 
-st.write("Ankündigungen, Infos, Tipps auf [https://discord.gg/V9pt9MZ6Ch](Discord).")
+st.write("Ankündigungen, Infos, Tipps auf dem Stoned Lack [Discord-Server](https://discord.gg/V9pt9MZ6Ch).")
 
 
 st.logo("Pictures/SL_logo.png", link=None, size='large')
