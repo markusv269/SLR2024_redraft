5d2ababa251d84604feac9ae1522950395015559
markusv269 <60115561+markusv269@users.noreply.github.com>
2025-02-12 20:11:27 +0000

    modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import streamlit as st
 
 st.title("Stoned Lack Redraft 2024 -- Wochenauswertung :football:")
-st.write(
+st.subheader(
     "Wöchentlicher Überblick über die SLR 2024. 35 Ligen, 420 Manager, eine App."
 )
 
-st.logo("Pictures/SL_logo", link=None, size='medium')
+st.write("Ankündigungen, Infos, Tipps auf Discord.")
+
+
+# st.logo("Pictures/SL_logo.png", link=None, size='medium')
