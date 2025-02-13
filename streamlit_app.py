18e909d33d110ceb43c07958749733edbb3ec97c
markusv269 <60115561+markusv269@users.noreply.github.com>
2025-02-13 08:36:45 +0000

    new file:   matchups.py 	modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,5 @@
 import streamlit as st
-import pandas as pd
-import os
-import json
+from matchups import matchups_df
 
 st.title("Stoned Lack Redraft 2024 -- Wochenauswertung :football:")
 st.subheader(
@@ -13,25 +11,8 @@ st.write("Ankündigungen, Infos, Tipps auf dem Stoned Lack [Discord-Server](http
 
 st.logo("Pictures/SL_logo.png", link=None, size='large')
 
-slr_league_ids = []
-weeks = list(range(1,18))
-matchups_df = pd.DataFrame()
+st.subheader("Matchups")
 
-for league_id in slr_league_ids:
-    for week in weeks:
-        file_path = f"matchups/{week}/{league_id}.json"
-        
-        if os.path.exists(file_path):
-            with open(file_path, "r") as file:
-                matchup_data = json.load(file)
-                
-                # Falls JSON-Daten eine Liste enthalten, direkt in DataFrame umwandeln
-                if isinstance(matchup_data, list):
-                    temp_df = pd.DataFrame(matchup_data)
-                else:
-                    temp_df = pd.DataFrame([matchup_data])
-                
-                # DataFrame mergen
-                matchups_df = pd.concat([matchups_df, temp_df], ignore_index=True)
+matchups_show = matchups_df
 
-print(matchups_df.head(5))
+st.dataframe(matchups_show, hide_index=True)
