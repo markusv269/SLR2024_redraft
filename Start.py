c234b81bac90d70decf6c87870d430d956f8ba02
markusv269 <60115561+markusv269@users.noreply.github.com>
2025-02-22 15:18:47 +0000

    modified:   Start.py 	deleted:    pages/0_Start.py

--- Start.py
+++ Start.py
@@ -1,5 +1,6 @@
-def main():
-    return None
-
-if __name__ == "__main__":
-    main()
+import streamlit as st
+st.set_page_config(page_title="Start", page_icon="🍎")
+st.title("Stoned Lack Redraft 2024 -- Wochenauswertung :football:")
+st.subheader("Wöchentlicher Überblick über die SLR 2024. 35 Ligen, 420 Manager, eine App.")
+st.write("Ankündigungen, Infos, Tipps auf dem Stoned Lack [Discord-Server](https://discord.gg/V9pt9MZ6Ch).")
+st.image("Pictures/SL_logo.png", width=150)
