78a97c5781101221a4c041c66cc3f9c1e8f1ac0c
markusv269 <60115561+markusv269@users.noreply.github.com>
2025-02-27 10:21:26 +0000

    modified:   Start.py 	modified:   views/0_start.py

--- Start.py
+++ Start.py
@@ -10,23 +10,6 @@ st.logo("Pictures/SL_logo.png")
 def feedback():
     return None
 
-if "userdf" not in st.session_state:
-    st.session_state['userdf'] = load_users()
-if "matchupsdf" not in st.session_state:
-    matchupsdf = load_matchups().merge(st.session_state["userdf"][['league_id', 'roster_id', 'display_name', 'league_name']], on=['league_id', 'roster_id'], how='left')
-    st.session_state["matchupsdf"] = matchupsdf
-if "rostersdf" not in st.session_state:
-    st.session_state["rostersdf"] = load_rosters()
-if "playersdf" and "playersdict" not in st.session_state:
-    st.session_state["playersdf"], st.session_state["playersdict"] = load_players()
-if "matchesdf" not in st.session_state:
-    st.session_state["matchesdf"] = get_matchup_results(
-        matchdf=st.session_state["matchupsdf"],
-        userdf=st.session_state["userdf"]
-        )
-if "scoring" not in st.session_state:
-    st.session_state["scoring"] = load_scoring_settings()
-
 # feedback_page = st.Page(feedback(), title="Feedback")
 
 start_page = st.Page(
@@ -89,3 +72,20 @@ pg = st.navigation(
 st.sidebar.write("Made by GoKingsGo", )
 
 pg.run()
+
+if "userdf" not in st.session_state:
+    st.session_state['userdf'] = load_users()
+if "matchupsdf" not in st.session_state:
+    matchupsdf = load_matchups().merge(st.session_state["userdf"][['league_id', 'roster_id', 'display_name', 'league_name']], on=['league_id', 'roster_id'], how='left')
+    st.session_state["matchupsdf"] = matchupsdf
+if "rostersdf" not in st.session_state:
+    st.session_state["rostersdf"] = load_rosters()
+if "playersdf" and "playersdict" not in st.session_state:
+    st.session_state["playersdf"], st.session_state["playersdict"] = load_players()
+if "matchesdf" not in st.session_state:
+    st.session_state["matchesdf"] = get_matchup_results(
+        matchdf=st.session_state["matchupsdf"],
+        userdf=st.session_state["userdf"]
+        )
+if "scoring" not in st.session_state:
+    st.session_state["scoring"] = load_scoring_settings()
--- views/0_start.py
+++ views/0_start.py
@@ -1,5 +1,34 @@
 import streamlit as st
 
 st.write('''
-    # Das Stoned Lack Universum    
+    # Das StonedLack Universum
+
+    ## About
+    Willkommen auf der Streamlit-Infoseite des StonedLack Fantasy Football Podcasts. 
+    
+    #### Stoned _what_ !?
+    StonedLack ist ein Live-Podcast der beiden Fantasy Football-Heads **Stoni** und **Lack** aus Wien.
+    Beinahe das ganze Jahr über betreiben sie den Podcast, der u. a. live auf dem [youtube-Kanal](https://www.youtube.com/@stonedlack) verfolgt werden kann.
+    Der Podcast ist auch auf allen gängigen Streaming-Plattformen verfügbar.
+    
+    Um den Podcast hat sich mittlerweile eine große Gemeinde Fantasy Football-Begeisterter versammelt, die den Podcast verfolgt, selbst aktiv einbringt etc. pp.
+    Fast aller Austausch in der Community findet auf dem Discord-Channel ([Einladungslink](https://discord.gg/V9pt9MZ6Ch)) statt.
+         
+    #### StonedLack Ligen
+    Seit vielen Jahren organisieren StonedLack eigene Ligen, in denen die Zuschauenden und -hörenden gegeneinander antreten. 
+    Jedes Jahr werden so bspw. Redraftligen organisiert, die dann live im Podcast ausgelost werden. 
+         
+    Des Weiteren gibt es viele Dynasty-Ligen, die über viele Jahre hinweg bespielt werden. 
+         
+    Die Gewinner jeder Liga spielen um die Marryo Larry Trophy und den Gesamtsieg.
+         
+    #### Schön und gut, aber was soll das hier?!
+    Gute Frage. Ich höre den Podcast seit gut drei Jahren. 
+    Um allen aus der Community einen Zugang zu den Redraftligen zu gewähren, habe ich vor 2 Jahren angefangen, die API der Plattform [sleeper](sleeper.com), 
+    auf der die Ligen organisiert sind, auszulesen.
+         
+    Angefangen hat alles mit der Idee, ein ADP-Draftboard aus allen Ligen zu generieren, also ein Spielerranking über alle Drafts der 2023er-StonedLack Redraftligen zu erstellen.
+    In der Folge habe ich begonnen, wöchentliche Updates zu allen Ligen in Discord zu schreiben. Motivation war auch, die Programmiersprache `python` besser kennen zu lernen.
+         
+    Nun mündet das Ganze in meinem vorerst größten und sichtbarstem Projekt, dieser Streamlit-Plattform.    
 ''')
