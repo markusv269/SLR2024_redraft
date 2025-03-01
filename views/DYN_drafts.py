a3477a2352e0edb5cd2c5f67d5a109ea673a3fe6
markusv269 <60115561+markusv269@users.noreply.github.com>
2025-03-01 20:48:52 +0000

    modified:   views/DYN_drafts.py

--- views/DYN_drafts.py
+++ views/DYN_drafts.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from Start import display_drafts
 
-# st.write('''
-#     # Dynasty Drafts''')
+st.write('''
+    # Dynasty Drafts''')
 
 dyn_leagues = st.session_state["dynleagues"]
 display_drafts(dyn_leagues)
