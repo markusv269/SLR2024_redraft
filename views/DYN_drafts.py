171a2548b2ef3be7b60614baa56e75d7ea3c1dbb
markusv269 <60115561+markusv269@users.noreply.github.com>
2025-03-10 09:25:20 +0000

    modified:   views/DYN_drafts.py 	modified:   views/RED_drafts.py

--- views/DYN_drafts.py
+++ views/DYN_drafts.py
@@ -1,8 +1,4 @@
 import streamlit as st
 from config import DYNLEAGUES
 from utils import display_drafts
-
-st.write('''
-    # Dynasty Drafts''')
-
 display_drafts(DYNLEAGUES)
--- views/RED_drafts.py
+++ views/RED_drafts.py
@@ -1,8 +1,4 @@
 import streamlit as st
 from config import REDLEAGUES
 from utils import display_drafts  # Falls du die Draft-Funktion ausgelagert hast
-
-st.write('''
-    # Dynasty Drafts''')
-
 display_drafts(REDLEAGUES)
