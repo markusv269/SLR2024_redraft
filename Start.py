e7b36480ba997dc6fd2bcb4a3daf9e12a41b2f77
markusv269 <60115561+markusv269@users.noreply.github.com>
2025-03-11 14:13:17 +0000

    modified:   Start.py

--- Start.py
+++ Start.py
@@ -14,7 +14,8 @@ def initialize_data():
         "playersdf": None,
         "playersdict": None,
         "matchesdf": None,
-        "scoring": SCORINGSETTINGS
+        "scoring": SCORINGSETTINGS,
+        "auth": None
     }
 
 if "session_data" not in st.session_state:
@@ -49,18 +50,57 @@ if st.session_state["session_data"]["matchesdf"] is None:
 st.image("Pictures/SL_logo.png", width=150)
 st.sidebar.write("by GoKingsGo, 2025")
 
+if st.session_state["session_data"]["auth"] is None:
+    st.session_state["session_data"]["auth"] = False
+
+login_data = st.secrets["airtable"]["login"]
+st.sidebar.subheader("Adminbereich")
+username = st.sidebar.text_input("Username")
+password = st.sidebar.text_input("Password", type="password")
+if st.sidebar.button("Login"):
+    if username in login_data and login_data[username] == password:
+        st.sidebar.success("Login erfolgreich!")
+        st.session_state["session_data"]["auth"] = True            
+    else:
+        st.sidebar.error("Falsche Zugangsdaten!")
+
 # Navigation
-pg = st.navigation(
+if st.session_state["session_data"]["auth"] == True:
+        pg = st.navigation(
+        {
+            "Start": [
+                st.Page(page="views/0_start.py", title="Das StonedLack Universum", icon=":material/home:", default=True),
+                st.Page(page="views/1_hottakes.py", title="Hot Takes-Sammlung", icon=":material/whatshot:"),
+                st.Page(page="views/2_champofchamps.py", title="Champ of Champs", icon=":material/trophy:"),
+                st.Page(page="views/3_coc_spiel.py", title="CoC Tippabgabe", icon=":material/casino:")
+            ],
+            "Redraft": [
+                st.Page(page="views/RED_slr2025.py", title="SLR2025 Anmeldung", icon=":material/login:"),
+                st.Page(page="views/RED_slr2025_status.py", title="SLR2025 Anmeldestatus", icon=":material/download_done:"),
+                st.Page(page="views/RED_info.py", title="Die Redraftligen", icon=":material/home:"),
+                st.Page(page="views/RED_uebersicht.py", title="Ligenübersicht", icon=":material/layers:"),
+                st.Page(page="views/RED_alte_Redrafts.py", title="Send your old SLR", icon=":material/send:"),
+                st.Page(page="views/RED_Wochenstatistiken.py", title="Wochenstatistiken", icon=":material/calendar_month:"),
+                st.Page(page="views/RED_Wochenkategorien.py", title="Wochenkategorien", icon=":material/bar_chart:"),
+                st.Page(page="views/RED_Matchups.py", title="Matchups", icon=":material/sports_football:"),
+                st.Page(page="views/RED_Manager.py", title="Manager", icon=":material/groups:"),
+                st.Page(page="views/RED_drafts.py", title="Drafts", icon=":material/target:")
+            ],
+            "Dynasty": [
+                st.Page(page="views/DYN_info.py", title="Dynasty", icon=":material/construction:"),
+                st.Page(page="views/DYN_drafts.py", title="Drafts", icon=":material/target:")
+            ]
+        }
+    )
+else:
+    pg = st.navigation(
     {
         "Start": [
             st.Page(page="views/0_start.py", title="Das StonedLack Universum", icon=":material/home:", default=True),
             st.Page(page="views/1_hottakes.py", title="Hot Takes-Sammlung", icon=":material/whatshot:"),
-            st.Page(page="views/2_champofchamps.py", title="Champ of Champs", icon=":material/trophy:"),
-            # st.Page(page="views/3_coc_spiel.py", title="CoC Tippabgabe", icon=":material/casino:")
+            st.Page(page="views/2_champofchamps.py", title="Champ of Champs", icon=":material/trophy:")
         ],
         "Redraft": [
-            # st.Page(page="views/RED_slr2025.py", title="SLR2025 Anmeldung", icon=":material/login:"),
-            # st.Page(page="views/RED_slr2025_status.py", title="SLR2025 Anmeldestatus", icon=":material/download_done:"),
             st.Page(page="views/RED_info.py", title="Die Redraftligen", icon=":material/home:"),
             st.Page(page="views/RED_uebersicht.py", title="Ligenübersicht", icon=":material/layers:"),
             st.Page(page="views/RED_alte_Redrafts.py", title="Send your old SLR", icon=":material/send:"),
@@ -73,8 +113,6 @@ pg = st.navigation(
         "Dynasty": [
             st.Page(page="views/DYN_info.py", title="Dynasty", icon=":material/construction:"),
             st.Page(page="views/DYN_drafts.py", title="Drafts", icon=":material/target:")
-        ],
-        # "Adminbereich": []
-    }
-)
+        ]
+    })
 pg.run()
